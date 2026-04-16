53ad562d2ebfc44d6198125ef4f7379729f24dfb
Sidhaant Singh Jyrwa <singhjyrwas@gmail.com>
2026-04-16 18:44:58 +0530

    Add files via upload

---
+++ Day16.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the number of elements in array\n");
+    scanf("%d",&n);
+
+    int arr[n];
+    int visited[n];   
+
+    printf("\nEnter the elements in array\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        visited[i] = 0;   
+    }
+
+    printf("\nFrequencies:\n");
+
+    for(int i=0;i<n;i++)
+    {
+        if(visited[i] == 1)
+            continue;
+
+        int count = 1;
+
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[i] == arr[j])
+            {
+                count++;
+                visited[j] = 1;  
+            }
+        }
+
+        printf("%d:%d ", arr[i], count);
+    }
+
+    return 0;
+}

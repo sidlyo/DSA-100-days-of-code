b9fefde683e9610e0ad72177ed904a7dc72b6e98
Sidhaant Singh Jyrwa <singhjyrwas@gmail.com>
2026-04-27 11:18:55 +0530

    Add files via upload

---
+++ Day39.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+int heap[MAX];
+int size = 0;
+
+// Swap function
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Heapify Up
+void heapifyUp(int i) {
+    while (i > 0 && heap[(i - 1) / 2] > heap[i]) {
+        swap(&heap[i], &heap[(i - 1) / 2]);
+        i = (i - 1) / 2;
+    }
+}
+
+// Heapify Down
+void heapifyDown(int i) {
+    int smallest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < size && heap[left] < heap[smallest])
+        smallest = left;
+
+    if (right < size && heap[right] < heap[smallest])
+        smallest = right;
+
+    if (smallest != i) {
+        swap(&heap[i], &heap[smallest]);
+        heapifyDown(smallest);
+    }
+}
+
+// Insert
+void insert(int x) {
+    if (size == MAX) return;
+
+    heap[size] = x;
+    heapifyUp(size);
+    size++;
+}
+
+// Extract Min
+void extractMin() {
+    if (size == 0) {
+        printf("-1\n");
+        return;
+    }
+
+    printf("%d\n", heap[0]);
+
+    heap[0] = heap[size - 1];
+    size--;
+
+    heapifyDown(0);
+}
+
+// Peek
+void peek() {
+    if (size == 0) {
+        printf("-1\n");
+    } else {
+        printf("%d\n", heap[0]);
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    char op[20];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%s", op);
+
+        if (strcmp(op, "insert") == 0) {
+            int x;
+            scanf("%d", &x);
+            insert(x);
+        } 
+        else if (strcmp(op, "extractMin") == 0) {
+            extractMin();
+        } 
+        else if (strcmp(op, "peek") == 0) {
+            peek();
+        }
+    }
+
+    return 0;
+}
